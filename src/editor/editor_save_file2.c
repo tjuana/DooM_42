5f1373dd2d692e79132ab24d0c6774e16969da8c
Twitch Juana <tjuana@oa-h6.21-school.ru>
2020-01-19 14:49:34 +0300

    space

--- src/editor/editor_save_file2.c
+++ src/editor/editor_save_file2.c
@@ -6,7 +6,7 @@
 /*   By: tjuana <tjuana@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/18 20:20:51 by tjuana            #+#    #+#             */
-/*   Updated: 2020/01/19 14:35:34 by tjuana           ###   ########.fr       */
+/*   Updated: 2020/01/19 14:49:17 by tjuana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,21 @@ void	ft_print_to_file(t_wolf3d *w, int f)
 {
 	char	*sy;
 	char	*sx;
+	char	*space_or_not;
 
 	sy = ft_strjoin(ft_strjoin("vertex\t", ft_itoa(w->file.i)), "\t");
-	sx = ft_strjoin(" ", ft_itoa(w->file.j));
+	space_or_not =ft_strdup(" ");
 	if (!f && w->file.count == 0)
+	{
 		ft_putstr_fd(sy, w->file.fd);
+		space_or_not = "";
+	}
 	else if (!f)
+	{
+		space_or_not = "";
 		ft_putstr_fd(ft_strjoin("\n", sy), w->file.fd);
+	}
+	sx = ft_strjoin(space_or_not, ft_itoa(w->file.j));
 	ft_putstr_fd(sx, w->file.fd);
 	ft_strdel(&sx);
 	ft_strdel(&sy);
