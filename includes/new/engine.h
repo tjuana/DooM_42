4908e4a2e38aaed3b76ab259a70446b5a70dbe27
Drahff Orange dartwing <dorange-@oa-i5.21-school.ru>
2020-02-22 13:42:18 +0300

    norme header (2)

--- includes/new/engine.h
+++ includes/new/engine.h
@@ -6,16 +6,13 @@
 /*   By: dorange- <dorange-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 21:14:55 by dorange-          #+#    #+#             */
-/*   Updated: 2020/02/21 21:14:58 by dorange-         ###   ########.fr       */
+/*   Updated: 2020/02/22 13:37:42 by dorange-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENGINE_H
 # define ENGINE_H
 # include "wolf3d.h"
-
-# define hfov (0.73f*WIN_H)
-# define vfov (0.2f*WIN_H)
 
 /*
 ** **************************************************************************
@@ -69,8 +66,9 @@ typedef struct		s_new_cycle
 **	float floor				| floor height
 **	floa ceil				| ceil height
 **	t_vector3 *vertex		| here stored all sector vertexes.
-**	signed char *neighbors	|  Each edge may have a corresponding neighboring sector
-**	int npoints				|  How many vertexes there are
+**	signed char *neighbors	| Each edge may have a corresponding
+**							| neighboring sector
+**	int npoints				| How many vertexes there are
 ** **************************************************************************
 */
 typedef struct		s_new_sector
@@ -99,29 +97,11 @@ typedef struct		s_new_xy
 **	int						|
 ** **************************************************************************
 */
-typedef struct	s_new_others
+typedef struct		s_new_others
 {
-    float	move_vec[2];//when player moves
-    int		moving;
-}			t_new_others;
-
-// typedef struct		s_new_others
-// {
-// 	const t_new_sector	*sect;
-//     const t_new_xy		*vert;
-//     t_new_xy 			p;
-//     t_new_xy			d;
-//     float			px;
-//     float			dx;
-//     float			py;
-//     float			dy;
-//     float			eye_h;
-//     float			hole_low;
-//     float			hole_high;
-// 	t_new_xy		dir_vec;
-//     //float			xd;
-//     //float			yd;
-// }					t_new_others;
+	float			move_vec[2];
+	int				moving;
+}					t_new_others;
 
 /*
 ** **************************************************************************
@@ -136,10 +116,14 @@ typedef struct	s_new_others
 **	int y2a					| ceil projection
 **	int ny1a				| projection neighbor
 **	int ny2a				| projection neighbor
-**	int cya					| Y coordinates for our ceil in this X coordinate (top)
-**	int cyb					| Y coordinates for our ceil in this X coordinate (bottom)
-**	int cnya				| Y coordinates for neighbor ceil in this X coordinate
-**	int cnyb				| Y coordinates for neighbor ceil in this X coordinate
+**	int cya					| Y coordinates for our ceil in this
+**							| X coordinate (top)
+**	int cyb					| Y coordinates for our ceil in this
+**							| X coordinate (bottom)
+**	int cnya				| Y coordinates for neighbor ceil in this
+**							| X coordinate
+**	int cnyb				| Y coordinates for neighbor ceil in this
+**							| X coordinate
 ** **************************************************************************
 */
 typedef struct		s_new_ceil
@@ -156,7 +140,6 @@ typedef struct		s_new_ceil
 	int				cnyb;
 }					t_new_ceil;
 
-
 /*
 ** **************************************************************************
 **	t_new_floor
@@ -166,16 +149,21 @@ typedef struct		s_new_ceil
 **	y - height of floor, n - neighbor, a - top, b - bottom
 **
 **	Arguments:
-**	float yfloor			| floor heights, relative to where the player's view is
+**	float yfloor			| floor heights, relative to where
+**							| the player's view is
 **	float nyfloor			| boundary between two neighbor sectors
-**	int y1b					| floor projection 
+**	int y1b					| floor projection
 **	int y2b					| floor projection
 **	int ny1b				| floor projection neighbor
 **	int ny2b				| floor projection neighbor
-**	int ya					| Y coordinates for our floor in this X coordinate
-**	int yb					| Y coordinates for our floor in this X coordinate
-**	int nya					| Y coordinates for neighbor floor in this X coordinate
-**	int nyb					| Y coordinates for neighbor floor in this X coordinate
+**	int ya					| Y coordinates for our floor in this
+**							| X coordinate
+**	int yb					| Y coordinates for our floor in this
+**							| X coordinate
+**	int nya					| Y coordinates for neighbor floor in this
+**							| X coordinate
+**	int nyb					| Y coordinates for neighbor floor in this
+**							| X coordinate
 ** **************************************************************************
 */
 typedef struct		s_new_floor
